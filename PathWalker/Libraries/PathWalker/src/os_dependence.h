8995886e32a9bcee128e5af097ffd7ecfd71ff37
Sergey <KillerOfFriend@yandex.ru>
2019-10-18 10:33:37 +0300

    18.10.2019 ASG FIX

--- PathWalker/Libraries/PathWalker/src/os_dependence.h
+++ PathWalker/Libraries/PathWalker/src/os_dependence.h
@@ -5,7 +5,8 @@
     #include <Windows.h>
     #define ENCODINGSETUP(inEncode) { SetConsoleCP(inEncode); SetConsoleOutputCP(inEncode); }
 #elif __linux__
-    #define ENCODINGSETUP() {}
+    #define CP_UTF8
+    #define ENCODINGSETUP(inEncode) {}
 #endif
 
 #endif // OS_DEPENDENCE_H
