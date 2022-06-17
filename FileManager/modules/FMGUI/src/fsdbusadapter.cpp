c594ecb20d2a5ba45832debb488fc2d21273efce
Sergey <killeroffriend@yandex.ru>
2022-06-17 21:50:43 +0300

    17.06.2022 ASG Fix

--- FileManager/modules/FMGUI/src/fsdbusadapter.cpp
+++ FileManager/modules/FMGUI/src/fsdbusadapter.cpp
@@ -14,33 +14,37 @@ FsDBusAdapter::FsDBusAdapter(QObject *inParent) :
 //-----------------------------------------------------------------------------
 void FsDBusAdapter::copy(const QString &inPath)
 {
-    auto result = m_qdbusInterface.copyObject(inPath);
+    auto result = m_qdbusInterface.copyObject(inPath); // Вызываем метод по DBus
+    result.waitForFinished(); // Ожидаем завершения выполнения метода
 
-    if (!result.isValid() || !result.value())
-        QMessageBox::warning(nullptr, tr("Error"), result.error().message());
+    if (!result.isValid() || !result.value()) // В случаи ошибки
+        QMessageBox::warning(nullptr, tr("Error"), result.error().message()); // Выводим соответствующее сообщение
 }
 //-----------------------------------------------------------------------------
 void FsDBusAdapter::cut(const QString &inPath)
 {
-    auto result = m_qdbusInterface.cutObject(inPath);
+    auto result = m_qdbusInterface.cutObject(inPath); // Вызываем метод по DBus
+    result.waitForFinished(); // Ожидаем завершения выполнения метода
 
-    if (!result.isValid() || !result.value())
-        QMessageBox::warning(nullptr, tr("Error"), result.error().message());
+    if (!result.isValid() || !result.value()) // В случаи ошибки
+        QMessageBox::warning(nullptr, tr("Error"), result.error().message()); // Выводим соответствующее сообщение);
 }
 //-----------------------------------------------------------------------------
 void FsDBusAdapter::paste(const QString &inPath)
 {
-    auto result = m_qdbusInterface.pasteObject(inPath);
+    auto result = m_qdbusInterface.pasteObject(inPath); // Вызываем метод по DBus
+    result.waitForFinished(); // Ожидаем завершения выполнения метода
 
-    if (!result.isValid() || !result.value())
-        QMessageBox::warning(nullptr, tr("Error"), result.error().message());
+    if (!result.isValid() || !result.value()) // В случаи ошибки
+        QMessageBox::warning(nullptr, tr("Error"), result.error().message()); // Выводим соответствующее сообщение
 }
 //-----------------------------------------------------------------------------
 void FsDBusAdapter::remove(const QString &inPath)
 {
-    auto result = m_qdbusInterface.deleteObject(inPath);
+    auto result = m_qdbusInterface.deleteObject(inPath); // Вызываем метод по DBus
+    result.waitForFinished(); // Ожидаем завершения выполнения метода
 
-    if (!result.isValid() || !result.value())
-        QMessageBox::warning(nullptr, tr("Error"), result.error().message());
+    if (!result.isValid() || !result.value()) // В случаи ошибки
+        QMessageBox::warning(nullptr, tr("Error"), result.error().message()); // Выводим соответствующее сообщение
 }
 //-----------------------------------------------------------------------------
